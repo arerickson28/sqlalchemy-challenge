1108d2430d922c680a535d14c57d69abb422cf35
Austen Rhyce Erickson <austenrerickson@Austens-MacBook-Pro-2.local>
2020-08-27 17:26:41 -0500

    saving progress

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -13,7 +13,10 @@ Base = automap_base()
 
 Base.prepare(engine, reflect=True)
 
-Climate = Base.classes.climate
+Climate = Base.classes.keys
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 app = Flask(__name__)
 
@@ -22,18 +25,20 @@ app = Flask(__name__)
 def welcome() :
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation"
-        f"/api/v1.0/stations"
-        f"/api/v1.0/tobs"
-        f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
     )
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
         session = Session(engine)
-        
+
+        last_year = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
+
         precip_results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= last_year)
     
         session.close()
@@ -47,6 +52,54 @@ def precipitation():
         
         return jsonify(precipitation)
 
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    stations =  session.query(Station.station).all()
+
+    session.close
+
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    last_year = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
+    tobs =  session.query(Measurement.station, Measurement.tobs).filter(Measurement.station == "USC00519397").filter(Measurement.date >= last_year).all()
+
+    session.close
+
+    return jsonify(tobs)
+
+
+
+
+@app.route("/api/v1.0/<start>")
+def start(start):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start, Measurement.station == "USC00519281").all()
+
+    session.close
+
+    return jsonify(results)
+
+
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date > start, Measurement.date < end, Measurement.station == "USC00519281").all()
+
+    session.close
+    return jsonify(results)
 
 
 
